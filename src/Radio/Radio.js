08ca8bbbbbf8351d3d5501d1fd45608c9a905df3
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2018-11-02 00:00:25 -0700

    Remove unused Radio

--- src/Radio/Radio.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const Radio = ({ id, children, labelProps, ...rest }) => (
-  <>
-    <input type="radio" id={id} name={id} {...rest} />
-    <label htmlFor={id}>{children}</label>
-  </>
-)
