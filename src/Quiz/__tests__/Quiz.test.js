4327a2318d5171feb377b728f77c66acd88fc2ba
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2018-11-01 23:40:10 -0700

    Fix tests

--- src/Quiz/__tests__/Quiz.test.js
+++ src/Quiz/__tests__/Quiz.test.js
@@ -9,48 +9,65 @@ const questions = [
   { question: 'Is this the fourth?' },
 ];
 
+const answers = ['nope', 'nah', 'meh', 'ok', 'yarp'];
+
 const props = {
   questions,
-  review: 'Review page mock'
-}
+  answers,
+  review: 'Review page mock',
+};
+
 describe('<Quiz />', () => {
   test('should render the first question', () => {
-    const { getByText } = render(<Quiz {...props} />)
-    expect(getByText('Is this the first?')).toBeInTheDocument()
+    const { container } = render(<Quiz {...props} />);
+    expect(container.querySelector('.isVisible')).toContainHTML(
+      'Is this the first?'
+    );
   });
 
   test('should advance each slide until the review slide', () => {
-    const { getByText } = render(<Quiz {...props} />);
+    const { getByText, container } = render(<Quiz {...props} />);
+
+    fireEvent.click(getByText('next'));
+    expect(container.querySelector('.isVisible')).toContainHTML(
+      'Is this the second?'
+    );
+
+    fireEvent.click(getByText('next'));
+
+    expect(container.querySelector('.isVisible')).toContainHTML(
+      'Is this the third?'
+    );
+
+    fireEvent.click(getByText('next'));
+    expect(container.querySelector('.isVisible')).toContainHTML(
+      'Is this the fourth?'
+    );
+
+    fireEvent.click(getByText('next'));
     
-    fireEvent.click(getByText('next'))
-    expect(getByText('Is this the second?')).toBeInTheDocument();
-
-    fireEvent.click(getByText('next'))
-    expect(getByText('Is this the third?')).toBeInTheDocument();
-
-    fireEvent.click(getByText('next'))
-    expect(getByText('Is this the fourth?')).toBeInTheDocument();
-
-    fireEvent.click(getByText('next'))
+    expect(container.querySelector('.isVisible')).toBeNull();
     expect(getByText('Review page mock')).toBeInTheDocument();
   });
 
   test('should not show back option if first slide', () => {
     const { queryByText } = render(<Quiz {...props} />);
     expect(queryByText('back')).toBeNull();
-  })
+  });
 
   test('should go back 1 slide until first slide', () => {
-    const { getByText } = render(<Quiz {...props} />);
-    
-    fireEvent.click(getByText('next'))
-    fireEvent.click(getByText('next'))
-    fireEvent.click(getByText('next'))
+    const { getByText, container } = render(<Quiz {...props} />);
 
-    fireEvent.click(getByText('back'))
-    fireEvent.click(getByText('back'))
-    fireEvent.click(getByText('back'))
-    
-    expect(getByText('Is this the first?')).toBeInTheDocument();
-  })
+    fireEvent.click(getByText('next'));
+    fireEvent.click(getByText('next'));
+    fireEvent.click(getByText('next'));
+
+    fireEvent.click(getByText('back'));
+    fireEvent.click(getByText('back'));
+    fireEvent.click(getByText('back'));
+
+    expect(container.querySelector('.isVisible')).toContainHTML(
+      'Is this the first?'
+    );
+  });
 });
