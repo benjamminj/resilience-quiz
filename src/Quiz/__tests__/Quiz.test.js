4b2b73c178d19de7414d2e5b434e32b2e3b86f66
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2018-11-02 00:28:41 -0700

    Update test

--- src/Quiz/__tests__/Quiz.test.js
+++ src/Quiz/__tests__/Quiz.test.js
@@ -29,23 +29,23 @@ describe('<Quiz />', () => {
   test('should advance each slide until the review slide', () => {
     const { getByText, container } = render(<Quiz {...props} />);
 
-    fireEvent.click(getByText('next'));
+    fireEvent.click(getByText('nope'));
     expect(container.querySelector('.isVisible')).toContainHTML(
       'Is this the second?'
     );
 
-    fireEvent.click(getByText('next'));
+    fireEvent.click(getByText('meh'));
 
     expect(container.querySelector('.isVisible')).toContainHTML(
       'Is this the third?'
     );
 
-    fireEvent.click(getByText('next'));
+    fireEvent.click(getByText('yarp'));
     expect(container.querySelector('.isVisible')).toContainHTML(
       'Is this the fourth?'
     );
 
-    fireEvent.click(getByText('next'));
+    fireEvent.click(getByText('ok'));
 
     expect(container.querySelector('.isVisible')).toBeNull();
     expect(getByText('Review page mock')).toBeInTheDocument();
@@ -59,9 +59,9 @@ describe('<Quiz />', () => {
   test('should go back 1 slide until first slide', () => {
     const { getByText, container } = render(<Quiz {...props} />);
 
-    fireEvent.click(getByText('next'));
-    fireEvent.click(getByText('next'));
-    fireEvent.click(getByText('next'));
+    fireEvent.click(getByText('ok'));
+    fireEvent.click(getByText('ok'));
+    fireEvent.click(getByText('ok'));
 
     fireEvent.click(getByText('back'));
     fireEvent.click(getByText('back'));
